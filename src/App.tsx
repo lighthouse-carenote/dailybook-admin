d0651f37082f20188e32a304fc4f359f906f021c
lighthouse-GE <gaeun.lee@lighthouse.love>
2023-11-20 17:02:37 +0900

    [파일 추가] system 페이지 컴포넌트 추가 및 라우팅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const DashboardPage = React.lazy(() => import("./pages/Home"));
 const LoginPage = React.lazy(() => import("./pages/Login"));
+const SystemPage = React.lazy(() => import("./pages/System"));
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<DashboardPage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/system" element={<SystemPage />} />
         </Routes>
       </Suspense>
     </BrowserRouter>
---
+++ src/pages/System/System.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const System = () => {
+  return <h1>Hello System!</h1>;
+};
+
+export default System;
