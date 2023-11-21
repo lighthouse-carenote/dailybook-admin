1ea1c44d59780c367bb7a34464a403816a526098
lighthouse-GE <gaeun.lee@lighthouse.love>
2023-11-21 16:43:31 +0900

    [코드 수정] Menu, MenuList UI 구현

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,15 +1,90 @@
 import React from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import {
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Divider,
+} from "@mui/material";
+import MenuList from "./MenuList";
 
 const Menu = () => {
-  const pathNames = useLocation();
+  // const pathNames = useLocation();
   const navigate = useNavigate();
 
   return (
     <div>
-      <div onClick={() => navigate("/")}>Dashboard</div>
-      <div onClick={() => navigate("/system")}>System</div>
-      <div onClick={() => navigate("/dailybook")}>Dailybook</div>
+      <List sx={{ width: "100%", maxWidth: 300 }}>
+        <ListItem disablePadding>
+          <ListItemButton component="a" onClick={() => navigate("/")}>
+            <ListItemText primary="Dashboard" />
+          </ListItemButton>
+        </ListItem>
+        <Divider />
+        <ListItem disablePadding>
+          <ListItemButton component="a" onClick={() => navigate("/#")}>
+            <ListItemText primary="System" />
+          </ListItemButton>
+        </ListItem>
+        <MenuList
+          data={[
+            { path: "/", label: "SMS/LMS 발송 (운영자용)" },
+            { path: "/", label: "Push Noti 발송 (개발자용)" },
+          ]}
+          menu={"공통기능"}
+        />
+        <MenuList
+          data={[
+            { path: "/", label: "공통 코드" },
+            { path: "/", label: "HTTP 요청 응답 이력" },
+            { path: "/", label: "스케줄러 관리" },
+          ]}
+          menu={"시스템 데이터"}
+        />
+        <Divider />
+        <ListItem disablePadding>
+          <ListItemButton component="a" onClick={() => navigate("/#")}>
+            <ListItemText primary="Dailybook" />
+          </ListItemButton>
+        </ListItem>
+        <MenuList
+          data={[
+            { path: "/", label: "시설장 요청 관리" },
+            { path: "/", label: "시설직원 요청 관리" },
+            { path: "/", label: "보호자 수기 가입" },
+          ]}
+          menu={"가입 승인 관리"}
+        />
+        <MenuList
+          data={[
+            { path: "/", label: "전체 게시글 통계" },
+            { path: "/", label: "공지사항" },
+            { path: "/", label: "알림장" },
+            { path: "/", label: "알림장 조회율" },
+          ]}
+          menu={"데이터 관리"}
+        />
+        <MenuList
+          data={[
+            { path: "/", label: "고객의 소리 관리" },
+            { path: "/", label: "사용자 관리" },
+            { path: "/", label: "사용자 스위칭" },
+            { path: "/", label: "시설 관리" },
+            { path: "/", label: "시설 상담" },
+            { path: "/", label: "시설 상담 이력" },
+          ]}
+          menu={"CRM 관리"}
+        />
+        <MenuList
+          data={[
+            { path: "/", label: "홈 배너 관리" },
+            { path: "/", label: "데일리북 소식 관리" },
+          ]}
+          menu={"앱 관리"}
+        />
+        <Divider />
+      </List>
     </div>
   );
 };
---
+++ src/components/Menu/MenuList.tsx
@@ -0,0 +1,111 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import { styled, ThemeProvider, createTheme } from "@mui/material/styles";
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import Paper from "@mui/material/Paper";
+import KeyboardArrowDown from "@mui/icons-material/KeyboardArrowDown";
+
+const FireNav = styled(List)<{ component?: React.ElementType }>({
+  "& .MuiListItemButton-root": {
+    paddingLeft: 16,
+    paddingRight: 16,
+    paddingTop: 12,
+    paddingBottom: 12,
+  },
+  "& .MuiListItemIcon-root": {
+    minWidth: 0,
+    marginRight: 16,
+  },
+  "& .MuiSvgIcon-root": {
+    fontSize: 20,
+  },
+});
+
+interface MenuListProps {
+  data: { path: string; label: string }[] | null;
+  menu: string;
+}
+const MenuList = ({ data, menu }: MenuListProps) => {
+  const [open, setOpen] = React.useState(false);
+  return (
+    <Box sx={{ display: "flex" }}>
+      <ThemeProvider
+        theme={createTheme({
+          components: {
+            MuiListItemButton: {
+              defaultProps: {
+                disableTouchRipple: true,
+              },
+            },
+          },
+          palette: {
+            mode: "light",
+            primary: { main: "rgb(102, 157, 246)" },
+          },
+        })}
+      >
+        <Paper elevation={0}>
+          <FireNav component="nav" disablePadding>
+            <Box
+              sx={{
+                bgcolor: open ? "rgba(0, 0, 0, 0.02)" : null,
+                pb: 0,
+                width: 300,
+              }}
+            >
+              <ListItemButton
+                alignItems="flex-start"
+                onClick={() => (data ? setOpen(!open) : null)}
+                sx={{
+                  px: 3,
+                  pt: 2.5,
+                  pb: 2.5,
+                  "& svg": { opacity: data ? 1 : 0 },
+                }}
+              >
+                <ListItemText
+                  primary={menu}
+                  primaryTypographyProps={{
+                    fontSize: 15,
+                    fontWeight: "bold",
+                    lineHeight: "20px",
+                    mb: "2px",
+                  }}
+                  sx={{ my: 0 }}
+                />
+                <KeyboardArrowDown
+                  sx={{
+                    mr: -1,
+                    opacity: 0,
+                    transform: open ? "rotate(-180deg)" : "rotate(0)",
+                    transition: "0.2s",
+                  }}
+                />
+              </ListItemButton>
+              {open &&
+                data &&
+                data.map((item) => (
+                  <ListItemButton
+                    key={item.label}
+                    sx={{ py: 0, minHeight: 48, color: "rgba(0,0,0)" }}
+                  >
+                    <ListItemText
+                      primary={item.label}
+                      primaryTypographyProps={{
+                        fontSize: 14,
+                        fontWeight: "medium",
+                      }}
+                    />
+                  </ListItemButton>
+                ))}
+            </Box>
+          </FireNav>
+        </Paper>
+      </ThemeProvider>
+    </Box>
+  );
+};
+
+export default MenuList;
