11e517af07f8a9ff8ef3346b07366878dd4614bb
lighthouse-GE <gaeun.lee@lighthouse.love>
2023-11-22 20:51:00 +0900

    [파일 추가] DailybookRegisterManual 파일 추가

---
+++ src/pages/Dailybook/DailybookRegisterManual/DailybookRegisterManual.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DailybookRegisterManual = () => {
+  return <>데일리북 레지스터 메뉴얼</>;
+};
+
+export default DailybookRegisterManual;
---
+++ src/pages/Dailybook/DailybookRegisterManual/index.ts
@@ -0,0 +1,1 @@
+export { default as DailybookRegisterManual } from "./DailybookRegisterManual";
