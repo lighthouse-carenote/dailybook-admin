e59e1d03a2aa55ff935face27d53277aabfb76a4
JiyoungSeo <jiyoung.seo@lighthouse.love>
2023-11-22 18:50:47 +0900

    [UI 추가] System/SMS, LMS 발송(운영자용)페이지 메세지 발송 컴포넌트 적용

---
+++ src/pages/System/SystemMessaging/SystemMessaging.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { SystemMessagingContent } from "~/components/System/SystemMessaging";
+import { Box } from "@mui/material";
+
+const SystemMessaging = () => {
+  return (
+    <Box>
+      <SystemMessagingContent />
+    </Box>
+  );
+};
+
+export default SystemMessaging;
