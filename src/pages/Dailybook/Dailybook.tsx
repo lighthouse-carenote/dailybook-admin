c6b86ade2b1c9c78f94b6a0e16368977a66c80f7
lighthouse-GE <gaeun.lee@lighthouse.love>
2023-11-22 19:19:01 +0900

    [코드 수정] Dailybook 페이지 컴포넌트에 dailybook 패스별 분기

--- src/pages/Dailybook/Dailybook.tsx
+++ src/pages/Dailybook/Dailybook.tsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import Layout from "~/components/Layout";
 
 const Dailybook = () => {
+  const pathNames = useLocation().pathname.split("/")[2];
   return (
     <Layout>
-      <div>Dailybook Here!</div>
+      {pathNames === "register" ? (
+        <div>register</div>
+      ) : pathNames === "staff_register" ? (
+        <div>staff_register</div>
+      ) : (
+        <div>register_manual</div>
+      )}
     </Layout>
   );
 };
