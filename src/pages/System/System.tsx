db1c00f46f326ada5c46d1d37d2274298c6472ee
JiyoungSeo <jiyoung.seo@lighthouse.love>
2023-11-23 16:03:56 +0900

    [코드 추가] System 페이지의 Push Noti 발송(개발자용) 페이지 추가

--- src/pages/System/System.tsx
+++ src/pages/System/System.tsx
@@ -2,13 +2,20 @@ import React from "react";
 import Layout from "~/components/Layout";
 import { useParams } from "react-router-dom";
 import SystemMessaging from "./SystemMessaging";
+import SystemPushMessage from "./SystemPushMessage";
 
 const System = () => {
   const { menu } = useParams();
 
   return (
     <Layout>
-      {menu === "messaging" ? <SystemMessaging /> : <>System Here!</>}
+      {menu === "messaging" ? (
+        <SystemMessaging />
+      ) : menu === "push_message" ? (
+        <SystemPushMessage />
+      ) : (
+        <></>
+      )}
     </Layout>
   );
 };
